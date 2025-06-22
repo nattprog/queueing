f4b63317f2c859bf4dd262ce2af73fd87d6135d7
skyking336 <3336jason@gmail.com>
2025-06-22 17:59:04 +0800

    Add files via upload
    input function

---
+++ set_peak_nonpeak.m
@@ -0,0 +1,22 @@
+function set_peak_nonpeak()
+    % Prompts user to choose peak or non-peak hours and sets global isNonPeak boolean
+    global isNonPeak;
+    
+    % Prompt user for peak or non-peak hours
+    response = input('Simulate non-peak hours? (1 for Yes, 0 for No): ');
+    
+    % Validate input
+    while response ~= 0 && response ~= 1
+        fprintf('Invalid input. Please enter 1 for non-peak or 0 for peak.\n');
+        response = input('Simulate non-peak hours? (1 for Yes, 0 for No): ');
+    end
+    
+    % Set boolean based on input
+    if response == 1
+        isNonPeak = true;
+        fprintf('Simulation set to non-peak hours.\n');
+    else
+        isNonPeak = false;
+        fprintf('Simulation set to peak hours.\n');
+    end
+end
