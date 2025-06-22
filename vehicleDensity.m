d5d64310db789a46a0cd77b6630ca83ef71b6f9a
Nathaniel Bong <nattprog7@gmail.com>
2025-06-22 23:16:07 +0800

    increase num of cars

--- vehicleDensity.m
+++ vehicleDensity.m
@@ -3,12 +3,12 @@ function p= vehicleDensity()
     global numOfVehicles isNonPeak interArrivalTime__vals interArrivalTime__vals__prob;
     
     if isNonPeak == false % Peak hours, more vehicles, faster arrival times
-        numOfVehicles = 15;
+        numOfVehicles = 50;
         interArrivalTime__vals = {0,1,2,3,4};
         interArrivalTime__vals__prob = {0.2,0.4,0.3,0.05,0.05};
         return
     elseif isNonPeak == true % Non-peak hours, less vehicles, more even arrival times
-        numOfVehicles = 7;
+        numOfVehicles = 15;
         interArrivalTime__vals = {2,3,4,5,6,7,8,9};
         interArrivalTime__vals__prob = {0.05,0.12,0.15,0.18,0.18,0.15,0.12,0.05};
         return
